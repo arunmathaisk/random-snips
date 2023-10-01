82890feb8d024a87df1f42fd4244fb081111c95c
Arun Mathai S.K <61804369+arunmathaisk@users.noreply.github.com>
2023-10-02 00:20:04 +0530

    Update and rename new_mahcine_install_script.sh to new_machine_install_script.sh

--- new_mahcine_install_script/new_machine_install_script.sh
+++ new_mahcine_install_script/new_machine_install_script.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 install_braveBrowser() {
+    echo "Updating package list..."
     sudo apt update -y
+    echo "Installing curl..."
     sudo apt install curl -y
 
+    echo "Adding Brave Browser keyring..."
     sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
+    echo "Adding Brave Browser repository..."
     echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
+    echo "Updating package list again..."
     sudo apt update -y
 
+    echo "Installing Brave Browser..."
     sudo apt install brave-browser -y
+    echo "Brave Browser installed!"
 }
 
 install_chrome() {
@@ -31,21 +38,38 @@ install_common_apps() {
 }
 
 install_vscode() {
+    echo "Adding Microsoft GPG key..."
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
     sudo mv packages.microsoft.gpg /usr/share/keyrings
 
+    echo "Adding VS Code repository..."
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 
+    echo "Updating package list..."
     sudo apt-get update -y
 
+    echo "Installing Visual Studio Code..."
     sudo apt-get install code -y
+    echo "Visual Studio Code installed!"
+}
+
+install_vscode_extensions() {
+    echo "Installing VS Code extensions..."
+    code --install-extension ms-python.python
+    code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+    code --install-extension ms-vscode.cpptools
+    code --install-extension esbenp.prettier-vscode
+    code --install-extension Vue.volar
+    echo "VS Code extensions installed!"
 }
 
 install_docker() {
+    echo "Updating package list..."
     sudo apt update -y
+    echo "Installing Docker and Docker Compose..."
     sudo apt install docker.io docker-compose -y
 
-    # Add the current user to the docker group
+    echo "Adding the current user to the docker group..."
     sudo usermod -aG docker $USER
 }
 
@@ -54,5 +78,5 @@ install_braveBrowser
 install_chrome
 install_common_apps
 install_vscode
+install_vscode_extensions
 install_docker
-#
