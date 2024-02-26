bb52d93178c97340f58841dd720a035eaf194303
Arun Mathai S.K <61804369+arunmathaisk@users.noreply.github.com>
2024-02-26 16:52:42 +0530

    Update new_machine_install_script.sh

--- new_machine_install_script/new_machine_install_script.sh
+++ new_machine_install_script/new_machine_install_script.sh
@@ -67,8 +67,22 @@ install_docker() {
     echo "Updating package list..."
     sudo apt update -y
     echo "Installing Docker and Docker Compose..."
-    sudo apt install docker.io docker-compose -y
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    
     echo "Adding the current user to the docker group..."
     sudo usermod -aG docker $USER
 }
